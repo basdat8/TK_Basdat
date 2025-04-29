4b84a4162819ba3ce0e2d5b0c9573ecec6d6d8f2
aliyahnhsa <aliyahnahisa@gmail.com>
2025-04-29 20:05:17 +0700

    added login & logout

---
+++ authuser/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Email atau password salah.')
+    return render(request, 'authuser/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
