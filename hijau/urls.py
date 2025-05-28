e2794021d91c32276c11d4d00a4b6ba549096c69
qqeel <ahmadaqeelknx@gmail.com>
2025-05-29 03:47:31 +0700

    working wow

--- hijau/urls.py
+++ hijau/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.list_catatan_medis, name='list_catatan_medis'),
     path('add/', views.add_catatan_medis, name='add_catatan_medis'),
-    path('edit/<uuid:id>/', views.edit_catatan_medis, name='edit_catatan_medis'),
-    path('delete/<uuid:id>/', views.delete_catatan_medis, name='hapus_catatan_medis'),
+    path('edit/', views.edit_catatan_medis, name='edit_catatan_medis'),
+    path('delete/', views.delete_catatan_medis, name='hapus_catatan_medis'),
 ]
--- hijau/views.py
+++ hijau/views.py
@@ -2,6 +2,7 @@ from django.http import Http404, HttpResponse
 from django.shortcuts import render, redirect
 from django.db import connection
 from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
 
 from kuning.views import dictfetchall
 
@@ -15,14 +16,17 @@ def list_catatan_medis(request, id):
         print(data)
     return render(request, 'catatanmedis_list.html', {'data': data, 'id': id})
 
+@login_required
 @require_POST
-def add_catatan_medis(request, id, username_dh):
+def add_catatan_medis(request, id):
     if request.method == 'POST':
         tanggal = request.POST['tanggal_pemeriksaan']
         status = request.POST['status_kesehatan']
         diagnosis = request.POST['diagnosis']
         pengobatan = request.POST['pengobatan']
         catatatan = request.POST['catatan_tindak_lanjut']
+        username_dh = request.user.username
+
         with connection.cursor() as cursor:
             cursor.execute("""
                 INSERT INTO catatan_medis (id_hewan, username_dh, tanggal_pemeriksaan, diagnosis, pengobatan, status_kesehatan, catatan_tindak_lanjut)
@@ -31,13 +35,16 @@ def add_catatan_medis(request, id, username_dh):
         return redirect('list_catatan_medis')
     return render(request, 'catatanmedis_form.html', {})
 
-def delete_catatan_medis(request, id, username_dh):
+# TODO: ADD DATE
+def delete_catatan_medis(request, id):
     with connection.cursor() as cursor:
-        cursor.execute("DELETE FROM catatan_medis WHERE id_hewan = %s AND username_dh = %s", [id, username_dh])
+        cursor.execute("DELETE FROM catatan_medis WHERE id_hewan = %s", [id])
     return redirect('list_catatan_medis')
 
-# TODO: ADD DATE
-def edit_catatan_medis(request, id, username_dh):
+@login_required
+@require_POST
+def edit_catatan_medis(request, id):
+    username_dh = request.user.username
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM catatan_medis WHERE id_hewan = %s AND username_dh = %s", [id, username_dh])
         data = dictfetchall(cursor)
